0a26eaf9e7aa13bfef7c9c126d5f4ffba87e465d
Samuel Comeau <24446727+sam-com@users.noreply.github.com>
2023-03-15 18:09:09 -0400

    superLongFunction

--- src/utils/wait.ts
+++ src/utils/wait.ts
@@ -1,2 +1,58 @@
 export const wait = async (ms: number) =>
   new Promise((resolve) => setTimeout(resolve, ms));
+
+export function superLongFunction() {
+  const a = 3;
+  if (a) {
+    console.log(a);
+    return a;
+  }
+  if (a) {
+    console.log(a);
+    return a;
+  }
+  if (a) {
+    console.log(a);
+    return a;
+  }
+  if (a) {
+    console.log(a);
+    return a;
+  }
+  if (a) {
+    console.log(a);
+    return a;
+  }
+  if (a) {
+    console.log(a);
+    return a;
+  }
+  if (a) {
+    console.log(a);
+    return a;
+  }
+  if (a) {
+    console.log(a);
+    return a;
+  }
+  if (a) {
+    console.log(a);
+    return a;
+  }
+  if (a) {
+    console.log(a);
+    return a;
+  }
+  if (a) {
+    console.log(a);
+    return a;
+  }
+  if (a) {
+    console.log(a);
+    return a;
+  }
+  if (a) {
+    console.log(a);
+    return a;
+  }
+}
